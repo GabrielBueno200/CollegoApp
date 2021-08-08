dbc070be6ef16158e22367a8d15db8b68156f54b
GabrielBueno200 <gabrielbueno200@gmail.com>
2021-08-08 17:27:34 -0300

    updated custom types

--- client-app/typings/custom.d.ts
+++ client-app/typings/custom.d.ts
@@ -1,11 +1,7 @@
-import { AppState } from "../app/store";
+import { compose } from 'redux';
 
 declare global {
     interface Window {
         __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
     }
 }
-
-declare module 'redux-thunk-dispatcher'{
-    type Dispatch<S> = ThunkDispatch<AppState, void, n>
-};

67e3ca156a8216220bc0f81c7dc26b2f3c7d9e0c
GabrielBueno200 <gabrielbueno200@gmail.com>
2021-08-07 22:03:01 -0300

    updated user regex

--- client-app/src/app/layout/components/UserRegisterForm/validation.ts
+++ client-app/src/app/layout/components/UserRegisterForm/validation.ts
@@ -4,7 +4,7 @@ import * as Yup from 'yup';
  * Masks
  */
 export const usernameFormat = (username: string) => 
-    `${username.replace(' ', '')}`.replace(/[-/$,:-@?{-~!"^`\[\]]/, "").toLowerCase();
+    username.replace(/[-/$\s,:-@?{-~!"^`\[\]]/, "").toLowerCase();
 
 export const fullnameFormat = (name: string) =>
     name.split(' ')
